a6c87db56ea312818c5463ae77879c31ea44e112
mashharuki <avp.104.106.107@ae.auone-net.jp>
2025-11-14 20:50:08 +0900

    fix

--- pkgs/frontend/src/app/api/chat/route.ts
+++ pkgs/frontend/src/app/api/chat/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const { city } = await req.json();
   // AI Agentを取得する
-  const agent = mastra.getAgent("x402Agent");
+  const agent = await mastra.getAgent("x402Agent");
   // AI Agentに処理を実行させる
   const result = await agent.stream(`What's the weather like in ${city}?`);
   console.log("result:", result);
